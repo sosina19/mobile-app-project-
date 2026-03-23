7c3462d8b80f3aa13e700250607521a68dde1e0d
Robel-drj <robeldrj1@gmail.com>
2026-03-23 07:49:36 -0700

    Adding Login and welcome page implementation

---
+++ lib/Login.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/admin.dart';
+import 'package:helloworld/student.dart';
+import 'Register.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  // Controllers for TextFields
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  // Login logic
+  void login() {
+    String username = usernameController.text.trim();
+    String password = passwordController.text.trim();
+
+    if (username == 'user' && password == 'user123') {
+      Future.delayed(Duration(seconds: 1), () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const UserPage()),
+        );
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Welcome User"),
+          backgroundColor: Colors.green,
+          duration: Duration(seconds: 1),
+        ),
+      );
+    } else if (username == 'admin' && password == 'admin123') {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Welcome Admin"),
+          backgroundColor: Colors.green,
+          duration: Duration(seconds: 1),
+        ),
+      );
+      Future.delayed(Duration(seconds: 1), () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const AdminPage()),
+        );
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Invalid username or password"),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context); // Go back to previous page
+          },
+        ),
+      ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          // prevents overflow on small screens
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 60),
+              Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Color(0xff0f0e0e),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 100),
+
+              // Username Field
+              TextField(
+                controller: usernameController,
+                decoration: InputDecoration(
+                  labelText: "Username",
+                  hintText: "Enter your Username",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  prefixIcon: Icon(Icons.person),
+                ),
+              ),
+              SizedBox(height: 20),
+
+              // Password Field
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  hintText: "Enter Your Password",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ),
+              SizedBox(height: 40),
+
+              // Login Button
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 10,
+                  shadowColor: Colors.black,
+                  backgroundColor: Color.fromARGB(255, 92, 146, 245),
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onPressed: login,
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 80),
+
+              // Sign Up Link
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an Account?",
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                  SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const Register(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/welcome.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'Login.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                '   \n\n\nWelcome  to Smart \n \u00A0\u00A0\u00A0\u00A0\u00A0Attendance',
+                style: TextStyle(
+                  fontSize: 35,
+                  letterSpacing: 1.5,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff080808),
+                ),
+              ),
+              SizedBox(height: 10),
+              /*Center(
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  width: 200,
+                  height: 200,
+                ),
+              ),*/
+              SizedBox(height: 200),
+              Text(
+                'Fast and Sequre Qr Attendance!',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: const Color.fromARGB(255, 211, 208, 208),
+                  letterSpacing: 2,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Get Started',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  letterSpacing: 1.5,
+                ),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(140, 50),
+                  elevation: 10,
+                  shadowColor: Colors.black,
+                  backgroundColor: const Color.fromARGB(255, 92, 146, 245),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Login()),
+                  );
+                },
+                child: Text(
+                  'Start',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                    fontFamily: 'Arial',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
